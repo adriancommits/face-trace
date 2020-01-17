5ecbddb6debc7458adfa3b5dcb83f5e9cb8648e6
adriancommits <apfelquark8@gmail.com>
2020-01-17 10:54:37 +0100

    Updates

---
+++ precache-manifest.81dff7b113b25df55c2ad8c9e60c1a88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c89fd214d35ee22c53556c81b0b27",
+    "url": "/face-trace/index.html"
+  },
+  {
+    "revision": "76b97334724efb7ccd8d",
+    "url": "/face-trace/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3bed872948069a47dbb",
+    "url": "/face-trace/static/js/2.453b5032.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-trace/static/js/2.453b5032.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76b97334724efb7ccd8d",
+    "url": "/face-trace/static/js/main.0b057eb8.chunk.js"
+  },
+  {
+    "revision": "a100f2b35761c1578334",
+    "url": "/face-trace/static/js/runtime-main.43ad2411.js"
+  }
+]);
