f33c00fa1a3c8e437efc1230e296d81ef536e05e
adriancommits <apfelquark8@gmail.com>
2020-01-17 10:46:09 +0100

    initial commit. created basic structure. only Navigation component created so far.

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// a component with no state. a pure function. Thus no class..
+
+const Navigation = () => {
+	return (
+		<nav>
+			<p>Sign Out</p>
+		</nav>
+	);
+}
+
+export default Navigation;
